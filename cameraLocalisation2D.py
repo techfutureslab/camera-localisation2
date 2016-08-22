bb8fd5e703544dd4d6ed96aa7409f356f821c941
mohsen <mohsen@techfutureslab.com>
2016-08-22 17:06:23 +1200

    Circles correctly drawn around found robots and now able to specify the calibration.

--- cameraLocalisation2D.py
+++ cameraLocalisation2D.py
@@ -185,6 +185,14 @@ class RobotDetector:
                     cv.destroyWindow(windowTitle)
                     break
 
+    def saveCalibration(self, filename):
+        print "colourHuesDegrees: ", self.colourHuesDegrees
+        print "colourHeusBandDegrees: ", self.colourHeusBandDegrees
+
+    def setCalibration(self, colourHuesDegrees, colourHeusBandDegrees):
+        self.colourHuesDegrees = colourHuesDegrees
+        self.colourHeusBandDegrees = colourHeusBandDegrees
+
     def findColouredPixels(self, hsvFrame, hueDegrees, hueBandDegrees=20):
         if hueDegrees - hueBandDegrees > 0 and hueDegrees + hueBandDegrees < 360:
             # The band does not wrap around 0/360 degrees
@@ -292,7 +300,7 @@ class RobotDetector:
         robot.orientation = math.degrees(math.atan2(deltaX, deltaY))
 
         # Debugging if requested
-        if True:
+        if debug:
             print "robot:",robot
 
     def updateLocations(self, frame, camera, debug=False):
@@ -301,15 +309,15 @@ class RobotDetector:
 
         for robot in self.robots:
             #if robot.location is None:
-                self.findRobot(robot, camera, debug)
+            self.findRobot(robot, camera, debug)
 
-        if True:
-            # Add circles around our robots
-            cv.circle(frame, robot.location, 20, (0,0,0), 2)
+            if True:
+                # Add circles around our robots
+                cv.circle(frame, robot.location, 20, (0,0,0), 2)
 
-            # Add a line to indicate the orientation
-            if robot.location is not None and robot.orientation is not None:
-                cv.line(frame, robot.location, (int(robot.location[0] + 20*math.sin(math.radians(robot.orientation))), int(robot.location[1] + 20*math.cos(math.radians(robot.orientation)))), (0,0,0), 3)
+                # Add a line to indicate the orientation
+                if robot.location is not None and robot.orientation is not None:
+                    cv.line(frame, robot.location, (int(robot.location[0] + 20*math.sin(math.radians(robot.orientation))), int(robot.location[1] + 20*math.cos(math.radians(robot.orientation)))), (0,0,0), 3)
 
 
 
@@ -319,7 +327,9 @@ class RobotDetector:
 
 fishEyeCamera = FishEyeCamera(0)
 robotDetector = RobotDetector(numberOfColours=3)
-robotDetector.calibrateColour(fishEyeCamera)
+# robotDetector.calibrateColour(fishEyeCamera)
+robotDetector.setCalibration([360.0, 140.5, 220.9], [15.7, 12.5, 8.6])
+robotDetector.saveCalibration("camera-calibration.txt")
 
 # m1, sd1 = robotDetector.getMeanAndStdDevFromColourSamples([2,1,252])
 # m2, sd2 = robotDetector.getMeanAndStdDevFromColourSamples([252,2,1])
@@ -346,8 +356,6 @@ while True:
     # robotDetector.findColouredBlobs(undistortedFrame, True)
 
 
-    # green = robotDetecter.findColouredPixels(hsvFrame, 2, 10) # Green
-    # cv.imshow("Green", green)
 
     wk = cv.waitKey(1)
     if wk != -1:
@@ -357,115 +365,3 @@ while True:
 
 cv.destroyAllWindows()
 exit()
-
-###########################################################################################################################
-
-
-
-
-while True:
-    startTime = time.time()
-    ret, frame = cam.read()
-    frame = cv.flip(cv.flip(frame, 1), -1)
-    #cv.imshow("Original", frame)
-    frame = cv.undistort(frame, camera_matrix, dist_coefs, None, newcameramtx)
-
-    # Convert to HSV
-    hsvFrame = cv.cvtColor(frame,cv.COLOR_BGR2HSV_FULL) #
-
-    robotDetecter = RobotDetector()
-    robotDetecter.findColouredPixels(hsvFrame, 240, 20) # Blue
-
-    key = waitKey(0)
-    continue
-
-    lb = np.array([156, 50,50])
-    ub = np.array([184,255,255])
-    maskBlue = cv.inRange(hsv_frame,lb, ub)
-    blu = cv.bitwise_and(frame,frame,mask=maskBlue)
-
-    lrz = np.array([0, 50,50])
-    lr = np.array([14, 255,255])
-    ur = np.array([241,50,50])
-    urf = np.array([255, 255,255])
-    maskRedLower = cv.inRange(hsv_frame,lrz, lr)
-    maskRedUpper = cv.inRange(hsv_frame,ur, urf)
-    maskRed = cv.bitwise_or(maskRedLower, maskRedUpper)
-    red = cv.bitwise_and(frame,frame,mask=maskRed)
-
-    lg = np.array([70, 50,50])
-    ug = np.array([100,255,255])
-    maskGreen = cv.inRange(hsv_frame,lg, ug)
-    grn = cv.bitwise_and(frame,frame,mask=maskGreen)
-
-    # cv.imshow("Logic Frames", (np.logical_and(hsv_frame[:,:,1]>110 , hsv_frame[:,:,1]<150)).astype('float'))
-    #cv.imshow("Red", red)
-    #cv.imshow("Green", grn)
-    #cv.imshow("Blue", blu)
-
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    # Detect red blobs
-    red_keypoints = detector.detect(red)
-    if len(red_keypoints) == 0:
-        continue
-    frame_with_keypoints = cv.drawKeypoints(frame, red_keypoints, np.array([]), (0, 0, 255),
-                                            cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # Detect green blobs
-    green_keypoints = detector.detect(grn)
-    if len(green_keypoints) ==0:
-        continue
-    frame_with_keypoints = cv.drawKeypoints(frame_with_keypoints, [green_keypoints[0]], np.array([]), (0, 255, 0),
-                                            cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # Calculate centre of robot
-    robotPositionPx = None
-    try:
-        grnPointX = green_keypoints[0].pt[0]
-        grnPointY = green_keypoints[0].pt[1]
-
-        redPointX = red_keypoints[0].pt[0]
-        redPointY = red_keypoints[0].pt[1]
-
-        deltaY = redPointY - grnPointY
-        deltaX = redPointX - grnPointX
-        acceptable_margin = 100 * 0.514  # (0.514 px /mm as measured 2016-08-11)
-        distanceBetweenMarkers = math.sqrt(deltaX**2 + deltaY**2)
-
-        if distanceBetweenMarkers>acceptable_margin:
-            print "Unacceptably far apart:", distanceBetweenMarkers, "mm"
-            continue
-
-        robotPositionPx = (int((redPointX + grnPointX)/2) , int((redPointY + grnPointY) / 2))
-        # print
-        # cv.rectangle(frame_with_keypoints, (robotPositionPx[0] - 5, robotPositionPx[1] - 5),
-        #              (robotPositionPx[0] + 5, robotPositionPx[1] + 5), 2, 2)
-
-        theta = math.degrees(math.atan2(deltaX,deltaY))
-        print "robotPositionPx", robotPositionPx, "Orientation is ", theta,  "delta x:", deltaX, "delta Y:", deltaY
-
-    except Exception as e:
-        print "Exception thrown"
-        print e
-
-    robotSize = 50
-
-    if robotPositionPx is not None:
-        cv.rectangle(frame_with_keypoints, (int(robotPositionPx[0]) - robotSize, int(robotPositionPx[1]) - robotSize),
-                     (int(robotPositionPx[0]) + robotSize, int(robotPositionPx[1]) + robotSize), 2, 2)
-
-    cv.imshow("Robot Location", frame_with_keypoints)
-
-    endTime = time.time()
-    print "frames/sec: ", 1/(endTime-startTime)
-
-    wk = cv.waitKey(1)
-    if wk != -1:
-        print wk
-        if wk == 1048608:
-            print("hsv:", hsv_frame[320, 240,0])
-            print("rgb:", frame[320, 240])
-    if wk == 1048689:
-        break
-
-cv.destroyAllWindows()
